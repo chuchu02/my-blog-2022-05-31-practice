a49956cab52fa62b95930a5237f7a587d39faf25
chuchu02 <rlaxogns73@gmail.com>
2022-05-31 19:17:25 +0900

    Head 컴포넌트 도입

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,11 @@
-import { Link } from "gatsby"
 import * as React from "react"
+import Head from "../components/head"
 
 // markup
 const IndexPage = () => {
   return (
     <main>
-
-<div style={{display:"flex", gap:20}}>
-      <Link to="/">홈</Link>
-      <Link to="/about">어바웃</Link>
-      <Link to="/test">테스트</Link>
-      <Link to="/testspace">테스트 스페이스</Link>
-      </div>
+      <Head />
       <title>Home Page</title>
       <h1>Home Page!!!</h1>
     </main>
