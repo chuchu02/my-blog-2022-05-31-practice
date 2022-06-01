d58620dca8ef255d1517f8d7d5c2815d0bd331af
chuchu02 <rlaxogns73@gmail.com>
2022-06-01 15:54:13 +0900

    test, test-space 제거, 레이아웃 꾸미기

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,9 +4,11 @@ import Layout from "../components/layout";
 function AboutPage() {
   return (
     
-    <Layout>
-      <title>자기소개</title>
-      <h1>자기소개 페이지 입니다.</h1>
+    <Layout pageTitle="About me">
+      <p>
+      Hi there! I'm the proud creator of this site, which I built with
+          Gatsby.
+      </p>
       </Layout>
     
     
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,8 @@ import Layout from "../components/layout"
 const IndexPage = () => {
   return (
     
-      <Layout>
-      <title>홈</title>
-      <h1>홈페이지</h1>
+      <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
       </Layout>
     
   )
