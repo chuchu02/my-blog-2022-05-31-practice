bc61f58289b8d899dd44eb37bf9259cf4aca5991
chuchu02 <rlaxogns73@gmail.com>
2022-06-02 20:51:13 +0900

    hero_image 필드가 파일필드로 인식되었기 때문에, 그에따른 수정

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -6,17 +6,25 @@ const BlogPost = ({ data }) => {
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <div>{data.mdx.frontmatter.hero_image.name}</div>
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   );
 };
+
 export const query = graphql`
   query ($id: String) {
     mdx(id: { eq: $id }) {
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
-      }
+        hero_image {
+          relativePath
+          name
+          ext
+          size
+        }
+            }
       body
     }
   }
